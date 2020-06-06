a6cc2d7682fb2c6943f52ef79fefb04218ef0bc5
Ben <swerdlowbenjamin>
2020-06-06 16:03:40 -0700

    backend and api done

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const fetch = require("node-fetch");
 
 admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
@@ -10,26 +11,58 @@ admin.initializeApp();
 // });
 
 exports.latLongToAddress = functions.firestore
-  .document("cleanups/")
+  .document("cleanups/{cleanup}")
   .onCreate(async (snapshot, context) => {
-      let data = snapshot.data();
-      let latitude = data.lat;
-      let longitude = data.long;
-      latitude = 34.0099;
-      longitude = 118.4960;
-      let reverseGeoencodingData = await fetch("https://maps.googleapis.com/maps/api/geocode/json?latlng="+latitude+","+longitude+"&key=AIzaSyDD-7OiQsQ_Ti0OIRbcjl8tI56OmR3xrMc");
-      let results = data.results;
-      let address = results.formatted_address;
-      let types = results.types;
+    let data = snapshot.data();
+    let latitude = data.lat;
+    let longitude = data.long;
+    latitude = 34.0099;
+    longitude = 118.496;
+    let reverseGeoencodingData = await fetch(
+      "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+        latitude +
+        "," +
+        longitude +
+        "&key=AIzaSyDD-7OiQsQ_Ti0OIRbcjl8tI56OmR3xrMc"
+    );
+    let reverseGeocodingResults = reverseGeoencodingData.results[0];
+    let address = reverseGeocodingResults.formatted_address;
+    let types = reverseGeocodingResults.types;
 
-      let place_id = results.place_id;
-      let placeData = await fetch("https://maps.googleapis.com/maps/api/place/details/json?placeid="+place_id+"&key=AIzaSyDD-7OiQsQ_Ti0OIRbcjl8tI56OmR3xrMc");
+    let place_id = reverseGeocodingResults.place_id;
+    let placeData = await fetch(
+      "https://maps.googleapis.com/maps/api/place/details/json?placeid=" +
+        place_id +
+        "&key=AIzaSyDD-7OiQsQ_Ti0OIRbcjl8tI56OmR3xrMc"
+    );
 
-      return snapshot.ref.update({
-          address: address,
-          types: types,
-      })
-    
+    let placeDataResults = placeData.result;
+    let name = placeDataResults.name;
+    let openNow = placeDataResults.opening_hours.open_now;
 
-      //AIzaSyDD-7OiQsQ_Ti0OIRbcjl8tI56OmR3xrMc
+    return snapshot.ref.update({
+      address: address,
+      types: types,
+      name: name,
+      openNow: openNow,
+    });
+
+    //AIzaSyDD-7OiQsQ_Ti0OIRbcjl8tI56OmR3xrMc
   });
+
+exports.getAllCleanups = functions.https.onRequest(async (req, res) => {
+  let documents = await admin
+    .firestore()
+    .collection("cleanups")
+    .listDocuments();
+
+
+  const promises = documents.map(async (value) => {
+   return (await value.get()).data();
+  });
+
+  const docs = await Promise.all(promises);
+  
+
+  return res.status(200).send(docs);
+});

724e138f2db993daf6b9252597e0b91f7c454499
jasonTelanoff <jason.telanoff@gmail.com>
2020-06-06 10:53:31 -0700

    Added Firebase

--- app/lib/homePage.dart
+++ app/lib/homePage.dart
@@ -1,8 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView();
+    return FutureBuilder(
+      future: Firestore.instance.collection('cleanups').getDocuments(),
+      builder: (c, s) {
+        if (s.connectionState != ConnectionState.done) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(),
+            ],
+          );
+        } else {
+          QuerySnapshot snapshot = s.data;
+          List documents = snapshot.documents;
+
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (c, i) => FlatButton(
+              onPressed: () {},
+              child: PlatformText(documents[i]['lat'].toString()),
+            ),
+          );
+        }
+      },
+    );
   }
 }

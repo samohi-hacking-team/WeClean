c93c66e22516a0be14a5ed54155ddcfcb566ee1d
jasonTelanoff <jason.telanoff@gmail.com>
2020-06-07 02:29:04 -0700

    no clue

--- firebase/src/index.js
+++ firebase/src/index.js
@@ -1,3 +1,42 @@
+import { GoogleMapsOverlay } from '@deck.gl/google-maps';
+import { HexagonLayer } from '@deck.gl/aggregation-layers';
+import { ScatterplotLayer } from '@deck.gl/layers';
+import { HeatmapLayer } from '@deck.gl/aggregation-layers';
+
+const url = 'https://us-central1-weclean-4af67.cloudfunctions.net/getAllCleanups';
+
+const scatterplot = () => new ScatterplotLayer({
+    id: 'scatter',
+    data: url,
+    opacity: 0.8,
+    filled: true,
+    radiusMinPixels: 2,
+    radiusMaxPixels: 5,
+    getPosition: d => [d.long, d.lat],
+    getFillColor: d => [255, 255, 0, 0],
+  });
+
+  const heatmap = () => new HeatmapLayer({
+    id: 'heat',
+    data: url,
+    getPosition: d => [d.long, d.lat],
+    getWeight: d => 1,
+    radiusPixels: 60,
+});
+
+const hexagon = () => new HexagonLayer({
+    id: 'hex',
+    data: url,
+    getPosition: d => [d.long, d.lat],
+    getElevationWeight: d => 1,
+    elevationScale: 100,
+    extruded: true,
+    radius: 1609,         
+    opacity: 0.6,        
+    coverage: 0.88,
+    lowerPercentile: 50
+});
+
 window.initMap = () => {
 
     const map = new google.maps.Map(document.getElementById('map'), {
@@ -5,4 +44,15 @@ window.initMap = () => {
         zoom: 5,
     });
     
+    const overlay = new GoogleMapsOverlay({
+        layers: [
+            scatterplot(),
+            heatmap(),
+            hexagon()
+        ],
+    });
+
+    
+    overlay.setMap(map);
+
 }

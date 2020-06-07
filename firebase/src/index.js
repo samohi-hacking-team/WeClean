1a52b688b62efdcbbe84528163036afbe4ba5cc6
Ben <swerdlowbenjamin>
2020-06-07 02:46:51 -0700

    congigure done

--- firebase/src/index.js
+++ firebase/src/index.js
@@ -1,58 +1,71 @@
-import { GoogleMapsOverlay } from '@deck.gl/google-maps';
-import { HexagonLayer } from '@deck.gl/aggregation-layers';
-import { ScatterplotLayer } from '@deck.gl/layers';
-import { HeatmapLayer } from '@deck.gl/aggregation-layers';
+import { GoogleMapsOverlay } from "@deck.gl/google-maps";
+import { HexagonLayer } from "@deck.gl/aggregation-layers";
+import { ScatterplotLayer } from "@deck.gl/layers";
+import { HeatmapLayer } from "@deck.gl/aggregation-layers";
 
-const url = 'https://us-central1-weclean-4af67.cloudfunctions.net/getAllCleanups';
-
-const scatterplot = () => new ScatterplotLayer({
-    id: 'scatter',
-    data: url,
+let getData = async () => {
+  const response = await fetch(
+    "https://us-central1-weclean-4af67.cloudfunctions.net/getAllCleanups"
+  );
+  console.log();
+  return response;
+};
+const scatterplot = (data) =>
+  new ScatterplotLayer({
+    id: "scatter",
+    data: data,
     opacity: 0.8,
     filled: true,
     radiusMinPixels: 2,
     radiusMaxPixels: 5,
-    getPosition: d => [d.long, d.lat],
-    getFillColor: d => [255, 255, 0, 0],
+    getPosition: (d) => [d.long, d.lat],
+    getFillColor: (d) => [255, 255, 0, 0],
   });
 
-  const heatmap = () => new HeatmapLayer({
-    id: 'heat',
-    data: url,
-    getPosition: d => [d.long, d.lat],
-    getWeight: d => 1,
+const heatmap = (data) =>
+  new HeatmapLayer({
+    id: "heat",
+    data: data,
+    getPosition: (d) => [d.long, d.lat],
+    getWeight: (d) => 1,
     radiusPixels: 60,
-});
+  });
 
-const hexagon = () => new HexagonLayer({
-    id: 'hex',
-    data: url,
-    getPosition: d => [d.long, d.lat],
-    getElevationWeight: d => 1,
+const hexagon = (data) =>
+  new HexagonLayer({
+    id: "hex",
+    data: data,
+    getPosition: (d) => {
+      console.log(d);
+      return [d.long, d.lat];
+    },
+    getElevationWeight: (d) => 1,
     elevationScale: 100,
     extruded: true,
-    radius: 1609,         
-    opacity: 0.6,        
+    radius: 1609,
+    opacity: 0.6,
     coverage: 0.88,
-    lowerPercentile: 50
-});
+    lowerPercentile: 50,
+  });
 
-window.initMap = () => {
+window.initMap = async () => {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 34.017980, lng: -118.476210},
+    zoom: 14,
+  });
 
-    const map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.0, lng: -100.0},
-        zoom: 5,
-    });
-    
-    const overlay = new GoogleMapsOverlay({
+  let data = await getData();
+  console.log(
+    data.json().then((data) => {
+      const overlay = new GoogleMapsOverlay({
         layers: [
-            scatterplot(),
-            heatmap(),
-            hexagon()
+          //scatterplot(data),
+          heatmap(data),
+          hexagon(data)
         ],
-    });
+      });
 
-    
-    overlay.setMap(map);
-
-}
+      overlay.setMap(map);
+    })
+  );
+};
